25052737640c2ff6f1620219252cd5a46bb0b1c4
Ranvinu Hewage <123268275+rhewage3@users.noreply.github.com>
2023-08-11 00:38:04 +0530

    fetching and displaying weather info

--- script.js
+++ script.js
@@ -24,4 +24,42 @@ async function search(ev){
 
 const debounceSearch = _.debounce(() => {search();}, 600);
 
+//getting weather data of the specific lon, lat and name from the API
+async function showWeather (lat, lon,name){
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}&units=metric&`);
+    const data = await response.json();
+    // console.log(data);
+
+
+    //getting all the data that given to vars
+    const temp = Math.round(data.main.temp);
+    const feelsLike = Math.round(data.main.feels_like);
+    const humidity = Math.round(data.main.humidity);
+    const wind = Math.round(data.wind.speed);
+    const icon = data.weather[0].icon;
+    // console.log({temp, feelsLike,humidity,wind,icon,data})
+
+    document.getElementById('city').innerHTML = name;
+    document.getElementById('degrees').innerHTML = temp + '&#8451;';
+    document.getElementById('feelsLikeValue').innerHTML = feelsLike + '<span>&#8451;</span>';
+    document.getElementById('windValue').innerHTML = wind + '<span>km/h</span>';
+    document.getElementById('humidityValue').innerHTML = humidity + '<span>%</span>';
+    document.getElementById('icon').src = `https://openweathermap.org/img/wn/${icon}@4x.png`;
+    document.querySelector('form').style.display = 'none';
+    document.getElementById('weather').style.display = 'block';
+
+
+}
+
 document.querySelector('input[type="text"]').addEventListener('keyup', debounceSearch);
+
+
+document.body.addEventListener('click', ev =>{
+    // console.log(ev);
+    const li = ev.target;
+    const {lat, lon, name} = li.dataset;
+    if(!lat){
+        return;
+    }
+    showWeather(lat, lon, name);
+});
